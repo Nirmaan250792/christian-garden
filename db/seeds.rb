2623e07cd563e17aa19e96cab1beb50fae1cb08b
Christian Bongard <christian.bongard90@gmail.com>
2022-05-12 21:29:59 +0400

    added the plant model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Cleaning the database
+Garden.destroy_all
+puts "Creating Gardens"
+
+Garden.create!(
+  name: "Jardin de Toto",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg"
+)
+
+Garden.create!(
+  name: "Jardin de petite fripouille",
+  banner_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg"
+)
+
+puts "Created #{Garden.count} gardens"
